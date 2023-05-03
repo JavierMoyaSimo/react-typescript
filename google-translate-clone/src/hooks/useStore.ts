29ac12bee3ff2f4552ce6827ca8fc248d69425c9
Javi <javiermoyasimo@gmail.com>
2023-05-03 17:39:28 +0200

    feat: all dispatch added in useStore()

--- google-translate-clone/src/hooks/useStore.ts
+++ google-translate-clone/src/hooks/useStore.ts
@@ -68,5 +68,36 @@ export function useStore() {
     const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
         useReducer(reducer, initialState);
 
-    return { fromLanguage, toLanguage, fromText, result, loading }
+    const interchangeLanguages = () => {
+        dispatch({ type: "INTERCHANGE_LANGUAGES" });
+    }
+
+    const setFromLanguage = (payload:string) => {
+        dispatch({ type: "SET_FROM_LANGUAGE", payload });
+    }
+
+    const setToLanguage = (payload:string) => {
+        dispatch({ type: "SET_TO_LANGUAGE", payload });
+    }
+
+    const setFromText = (payload:string) => {
+        dispatch({ type: "SET_FROM_TEXT", payload });
+    }
+
+    const setResult = (payload:string) => {
+        dispatch({ type: "SET_RESULT", payload })
+    }
+
+    return {
+        fromLanguage,
+        toLanguage,
+        fromText,
+        result,
+        loading,
+        interchangeLanguages,
+        setFromLanguage,
+        setToLanguage,
+        setFromText,
+        setResult
+    }
 }
