7ff19433a5c503958f22c5e5ac8ad4fcb2b428c5
Javi <javiermoyasimo@gmail.com>
2023-05-17 17:39:59 +0200

    feat: Import container, row and col of bootstrap. Basic web content organitation

--- google-translate-clone/src/App.tsx
+++ google-translate-clone/src/App.tsx
@@ -1,26 +1,31 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useStore } from "./hooks/useStore";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   //Importamos el useStore de nuestros customs hooks.
   //Con él, traemos el fromLanguage y el setFromLanguage para hacer el dispatch de la accion con el payload "es"
-  const { fromLanguage, setFromLanguage } = useStore();
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore();
 
   return (
-    <>
-      <div>
-        <h1>Google Translate</h1>
-        <button
-          onClick={() => {
-            setFromLanguage("es");
-          }}
-        >
-          Cambiar a español
-        </button>
-        {fromLanguage}
-      </div>
-    </>
+    <Container fluid>
+      <h1>Google Translate</h1>
+
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+        <Col>
+          <button onClick={interchangeLanguages}>Intercambiar</button>
+        </Col>
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
