6e415c76173d35667bec178100a2043507873d55
Javi <javiermoyasimo@gmail.com>
2023-05-22 20:28:25 +0200

    feat: Create LanguageSelector component.In App.tsx, first Col and last Col modified to <LanguageSelector/>

--- google-translate-clone/src/App.tsx
+++ google-translate-clone/src/App.tsx
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useStore } from "./hooks/useStore";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { AUTO_LANGUAGE } from "./constants";
-import { ArrowsIcon } from "./components/Icons.tsx/Icons";
+import { ArrowsIcon } from "./components/Icons";
+import { LanguageSelector } from "./components/LanguageSelector";
 
 function App() {
   //Importamos el useStore de nuestros customs hooks.
@@ -16,11 +17,11 @@ function App() {
 
       <Row>
         <Col>
-          <h2>From</h2>
-          {fromLanguage}
+          <LanguageSelector />
         </Col>
         <Col>
           <Button
+            variant="link"
             disabled={fromLanguage === AUTO_LANGUAGE}
             onClick={interchangeLanguages}
           >
@@ -28,8 +29,7 @@ function App() {
           </Button>
         </Col>
         <Col>
-          <h2>To</h2>
-          {toLanguage}
+          <LanguageSelector />
         </Col>
       </Row>
     </Container>
