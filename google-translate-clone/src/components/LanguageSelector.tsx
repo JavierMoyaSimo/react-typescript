ed6624eb8e7f59ec4693e76626f8993e7f6a8662
Javi <javiermoyasimo@gmail.com>
2023-05-23 19:33:39 +0200

    feat: handleChange added in LanguageSelector.

--- google-translate-clone/src/components/LanguageSelector.tsx
+++ google-translate-clone/src/components/LanguageSelector.tsx
@@ -1,14 +1,21 @@
 import { Form } from "react-bootstrap";
 import { SUPPORTED_LANGUAGES } from "../constants";
+import { Language } from "../types";
 
 //void significa que no devuelve nada
 interface Props {
   onChange: (language: string) => void;
 }
 
+
 export const LanguageSelector = ({ onChange }: Props) => {
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(event.target.value as Language)
+  }
+
   return (
-    <Form.Select aria-label="Selecciona el idioma">
+    <Form.Select aria-label="Selecciona el idioma" onChange={handleChange}>
       {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
         <option key={key} value={key}>
           {literal}
